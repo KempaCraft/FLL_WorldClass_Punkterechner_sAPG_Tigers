a05965c6315933436b03bee8539bc40d1549937f
Jonas D <jonas.drotleff@gmail.com>
2014-08-18 18:11:35 +0200

    Test
    Added a test class
    Result: Doesn't work :/

---
+++ FLL_WorldClass_Punkterechner/src/test/gui/TestClock.java
@@ -0,0 +1,51 @@
+package test.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import de.time.TimePanel;
+
+public class TestClock extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestClock frame = new TestClock();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TestClock() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setExtendedState(MAXIMIZED_BOTH);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
+		
+		TimePanel tp = new TimePanel();
+		tp.setBounds(450, 194, 450,	300);
+		contentPane.add(tp);
+	}
+
+}
