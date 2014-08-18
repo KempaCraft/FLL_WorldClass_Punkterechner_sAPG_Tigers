8259bd0770ee4c3a5a261b162ed7dd074dcb7d17
KempaCraft <julius.eyermann@web.de>
2014-08-18 18:54:11 +0200

    Whileschleif

--- FLL_WorldClass_Punkterechner/src/de/time/Timemanager.java
+++ FLL_WorldClass_Punkterechner/src/de/time/Timemanager.java
@@ -34,42 +34,45 @@ public class TimeManager extends Thread{
 	}
 	
 	public void run(){
-		while(runTimer){
-			try {
-				Thread.sleep(1000);} catch (Exception e) {e.printStackTrace();}
-			up_sec++;
-			down_sec--;
-			
-				if(up_sec==60){			// 00:58  -  00:59  -  01:00
-					up_sec = 0;
-					up_min++;
-				}
-				if(down_sec==-1){		// 02:01  -  02:00  -  01:59
-					down_sec = 59;
-					down_min--;
-				}
-			
-				if(!runTimerDown){
-					if(up_sec<10){
-						TimePanel.lbl_time.setText("0" + up_min + ":0" + up_sec);		// up_sec<10  ->  00:09
-					}else{
-						TimePanel.lbl_time.setText("0" + up_min + ":" + up_sec);		// up_sec>10  ->  00:11
-					}
-					TimePanel.lbl_time_rest.setText("02:30");
-				}else{
-					if(down_sec<10){
-						TimePanel.lbl_time.setText("0" + down_min + ":0" + down_sec);		// up_sec<10  ->  00:09
-					}else{
-						TimePanel.lbl_time.setText("0" + down_min + ":" + down_sec);		// up_sec>10  ->  00:11
-					}
-					TimePanel.lbl_time_rest.setText("00:00");
-				}
+		while(true){
+			while(runTimer){
+				try {
+					Thread.sleep(1000);} catch (Exception e) {e.printStackTrace();}
+				up_sec++;
+				down_sec--;
 				
-				if(down_min == 0 && down_sec == 0){
-					runTimer=false;
-					TimePanel.btn_start.setSelected(false);
-				}
-			
+					if(up_sec==60){			// 00:58  -  00:59  -  01:00
+						up_sec = 0;
+						up_min++;
+					}
+					if(down_sec==-1){		// 02:01  -  02:00  -  01:59
+						down_sec = 59;
+						down_min--;
+					}
+				
+					if(!runTimerDown){
+						if(up_sec<10){
+							TimePanel.lbl_time.setText("0" + up_min + ":0" + up_sec);		// up_sec<10  ->  00:09
+						}else{
+							TimePanel.lbl_time.setText("0" + up_min + ":" + up_sec);		// up_sec>10  ->  00:11
+						}
+						TimePanel.lbl_time_rest.setText("02:30");
+					}else{
+						if(down_sec<10){
+							TimePanel.lbl_time.setText("0" + down_min + ":0" + down_sec);		// up_sec<10  ->  00:09
+						}else{
+							TimePanel.lbl_time.setText("0" + down_min + ":" + down_sec);		// up_sec>10  ->  00:11
+						}
+						TimePanel.lbl_time_rest.setText("00:00");
+					}
+					
+					if(down_min == 0 && down_sec == 0){
+						runTimer=false;
+						TimePanel.btn_start.setSelected(false);
+					}
+				
+			}
 		}
+		
 	}
 }
